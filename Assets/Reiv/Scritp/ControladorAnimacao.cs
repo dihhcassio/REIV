dfcb2396d076538a3de285be50cc0da04ebec995
dihhcassio <adricarr@gmail.com>
2014-05-30 14:43:29 -0300

    Controles da camera, e controle de animação.

---
+++ Assets/Reiv/Scritp/ControladorAnimacao.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControladorAnimacao : MonoBehaviour {
+
+
+	public Animator animator;
+
+	private ThirdPersonController controller;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<ThirdPersonController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (controller.Walking) 
+			animator.SetFloat ("velocidade", 1.0F);
+		else
+			animator.SetFloat ("velocidade", 0.0F);
+	}
+}
